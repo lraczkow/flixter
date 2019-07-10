80a5e52b20a84c2cbaa737dd35c236d521524fac
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-10 18:57:32 +0000

    video uploading

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,10 +10,5 @@ CarrierWave.configure do |config|
       secret_access_key: ENV["AWS_SECRET_KEY"],
       region:            ENV["AWS_REGION"]
   }
-
-  if Rails.env.development?
-    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
-    config.root = '/home/vagrant/uploads_tmp/tmp'
-  end
 end
 
