591625a69d1f2acf0c90318ed9f69ac2028e175e
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-22 13:29:41 +0000

    new lesson form modal

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,8 @@ Rails.application.routes.draw do
   namespace :instructor do
     # these two are for the drag-and-drop AJAX POST request routes
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    
-    resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
